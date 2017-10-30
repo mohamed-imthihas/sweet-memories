84ddf23fa1de3682ca3f3710ce80c2179fe8276e
Mohamed Imthihas <imthihas94@gmail.com>
2017-10-30 12:47:03 +0530

    Added slider to header and customer review

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
     $.scrollSpeed(100, 800);
     $(".button-collapse").sideNav();
+    $('.carousel.carousel-slider').carousel({ fullWidth: true, duration: 100 });
+    $('.slider').slider({ indicators: false });
 });
